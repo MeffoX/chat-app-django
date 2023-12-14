fbf8938cc0b333b7a354016ec448d771dbcb63f2
StefanRoth <roth.neukirchen@googlemail.com>
2023-12-14 10:18:24 +0100

    create channel when none exist

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -6,45 +6,44 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core import serializers
 from django.contrib.auth import logout
+from django.db import transaction
 from django.contrib import messages
 
 @login_required(login_url='/login/')
 def index(request):
     """
         Main chat view. Requires login to access.
-
-        On POST request: Receives a message and saves it to the database. Returns the serialized message as JSON.
-        On GET request: Retrieves all messages for a specific chat and renders them in the chat template.
+        On POST request: Receives a message and saves it to the database.
+        On GET request: Retrieves all messages for the chat and renders them in the chat template.
     """
+    chat = Chat.objects.first() 
+
     if request.method == 'POST':
-        print("Received data" + request.POST['textmessage'])
-        myChat = Chat.objects.get(id=1)
-        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        new_message = Message.objects.create(
+            text=request.POST['textmessage'], 
+            chat=chat, 
+            author=request.user, 
+            receiver=request.user
+        )
         serialized_obj = serializers.serialize('json', [new_message, ])
         return JsonResponse(serialized_obj[1:-1], safe=False)
     
-    chatMessages = Message.objects.filter(chat__id=1)
+    chatMessages = Message.objects.filter(chat=chat)
     return render(request, 'chat/index.html', {'messages': chatMessages})
-
 
 def login__view(request):
     """
         Handles user login.
-
-        On POST request: Authenticates the user and redirects to the next page or chat view.
-        On GET request: Renders the login page.
     """
     redirect = request.GET.get('next', '/chat/')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-
         if user:
             login(request, user)
-            return HttpResponseRedirect(request.POST.get('redirect'))
+            return HttpResponseRedirect(redirect)
         else:
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
-
 
 def logout_view(request):
     """
@@ -53,13 +52,10 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/login/')
 
-
 def register__view(request):
     """
         Handles new user registration.
-
-        On POST request: Registers a new user and redirects to the chat view.
-        On GET request: Renders the registration page.
+        Create a new Chat when none exists
     """
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -72,7 +68,10 @@ def register__view(request):
             messages.error(request, 'Email already exists')
             return render(request, 'auth/register.html')
 
-        user = User.objects.create_user(username=username, email=email, password=password)
+        with transaction.atomic():
+            user = User.objects.create_user(username=username, email=email, password=password)
+            if not Chat.objects.exists():
+                Chat.objects.create()
         login(request, user)
         return HttpResponseRedirect('/chat/')
 
